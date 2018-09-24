a136270338f1d25c069059ef98cc570652db9399
Oscar Sandoval <apollo.sandoval@gmail.com>
2018-09-24 10:56:30 -0700

    added pallete eventListener

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 document.addEventListener("DOMContentLoaded", function(e) {
     let canvas = document.querySelector('.canvas');
+    let palette = document.querySelector('.palette');
 
     let changeColor = function(event) {
+        if (!event.target.classList.contains('pixel')) {
+            return;
+        }
         let pixel = event.target;
         pixel.classList.toggle('red');
     };
 
+    let onPaletteClick = function(event) {
+        if (!event.target.classList.contains('color')) {
+            return;
+        }
+        alert(`You clicked ${event.target.id}`);
+    }
+
     canvas.addEventListener('click', changeColor);
+    palette.addEventListener('click', onPaletteClick)
 });
