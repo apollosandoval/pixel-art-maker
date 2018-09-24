08bc56d5ba52a821d833904a6be333dc73f3b6ae
Oscar Sandoval <apollo.sandoval@gmail.com>
2018-09-24 12:06:38 -0700

    working pixel art maker

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
 document.addEventListener("DOMContentLoaded", function(e) {
     let canvas = document.querySelector('.canvas');
     let palette = document.querySelector('.palette');
+    let currentColor = palette.querySelector('#color-picker');
 
     let changeColor = function(event) {
         if (!event.target.classList.contains('pixel')) {
             return;
         }
         let pixel = event.target;
-        pixel.classList.toggle('red');
+        pixel.setAttribute('class', 'pixel ' + currentColor.classList.value);
     };
 
     let onPaletteClick = function(event) {
         if (!event.target.classList.contains('color')) {
             return;
         }
-        alert(`You clicked ${event.target.id}`);
+        currentColor.setAttribute('class', event.target.id);
     }
 
     canvas.addEventListener('click', changeColor);
